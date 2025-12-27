29f181c3b3ba0ba858a303bd02565013174874d2
bala-amypo <bala@amypo.in>
2025-12-27 05:45:51 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,13 +1,17 @@
 package com.example.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class AuthRequest {
+    
+    @Schema(example = "developer@example.com")
     @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required")
     private String email;
 
+    @Schema(example = "SecurePassword123")
     @NotBlank(message = "Password is required")
     private String password;
 
